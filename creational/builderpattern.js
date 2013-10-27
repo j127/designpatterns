6527df2b61db998febae215c916ab851ff71367d
J. Cohen <hostelmarketing@gmail.com>
2013-10-27 12:11:49 -0700

    Create the basic structure

---
+++ creational/builderpattern.js
@@ -0,0 +1,5 @@
+// Applying the builder pattern allows us to construct objects by only specifying the type and the content of the object. We don’t have to explicitly create the object.
+
+// Gets a jQuery DOM object
+var tableCells = $('td');
+
